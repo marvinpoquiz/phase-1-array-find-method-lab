f9245dff7b9a22d31d7229326ec52c975fa19fe1
Marvin <marvinepoquiz@gmail.com>
2021-11-10 12:48:02 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function superbowlWin(record) {
+    const possibleWin = record.find(object => object.result === 'W')
+    return possibleWin ? possibleWin.year : undefined
+} 
 
+
+
+
+
+//function sadReality (element) {
+ //   if(element !== 'W') {
+   //     if(element !== 'W') {
+   // return undefined
+  ///}
+//}
